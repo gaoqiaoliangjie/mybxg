3e9cf19893991b98c59480019e27e23bc3ea0c86
code1031abc <1031247267@qq.com>
2017-09-27 21:52:00 +0800

    添加表单验证和提交插件

--- public/js/config.js
+++ public/js/config.js
@@ -7,11 +7,13 @@ require.config({
         bootstrap:'bootstrap/js/bootstrap.min',
         datepicker:'bootstrap-datepicker/js/bootstrap-datepicker',
         language:'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',
+        validate:'validate/jquery-validate',
+        form:'jquery-form/jquery.form',
         util:'../js/util',
         common: '../js/common',
         login: '../js/login',
         teacher:'../js/teacher',
-        teacheradd:'../js/teacher-add',
+        teacheradd:'../js/teacher-add'
 
     },
     shim:{
@@ -20,6 +22,9 @@ require.config({
         },
         language:{
             deps:['jquery','datepicker']
+        },
+        validate:{
+            deps:['jquery']
         }
     }
 
--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -1,56 +1,90 @@
-define(['jquery','template','util','datepicker','language'],function($,template,util){
-    var tcId=util.qs('tc_id');
+define(['jquery', 'template', 'util', 'datepicker', 'language','validate','form'], function ($, template, util) {
+    var tcId = util.qs('tc_id');
     //console.log(tcId)
-    if(tcId){
+    if (tcId) {
         //编辑讲师
         $.ajax({
-            type:'get',
-            url:'/api/teacher/edit',
-            data:{tc_id:tcId},
-            dataType:'json',
-            success:function(data){
+            type: 'get',
+            url: '/api/teacher/edit',
+            data: {tc_id: tcId},
+            dataType: 'json',
+            success: function (data) {
                 //console.log(data)
-                data.result.operate='编辑讲师'
-                var html=template('teacheraddTpl',data.result)
+                data.result.operate = '编辑讲师'
+                var html = template('teacheraddTpl', data.result)
                 $('#teacheraddInfo').html(html)
 
-            //    处理表单提交
+                //    处理表单提交
                 submitForm('/api/teacher/update')
 
             }
 
         })
 
-    }else{
-    //    添加讲师
-        var html=template('teacheraddTpl',{operate:'添加讲师'})
+    } else {
+        //    添加讲师
+        var html = template('teacheraddTpl', {operate: '添加讲师'})
         $('#teacheraddInfo').html(html)
         submitForm('/api/teacher/add')
     }
-
-//    实现表单提交
-    function submitForm(url){
-        $('#teacherBtn').click(function(){
-            $.ajax({
-                type:'post',
-                url:url,
-                data:$('#teacherForm').serialize(),
-                dataType:'json',
-                success:function(data){
-                    if(data.code==200){
-                        location.href='/teacher/teacher_list'
+    //实现表单提交
+    function submitForm(url) {
+        $('#teacherForm').validate({
+            sendForm: false,
+            valid: function () {
+                //console.log('success')
+                $(this).ajaxSubmit({
+                    type:'post',
+                    url:url,
+                    dataType:'json',
+                    success:function(data){
+                        console.log(data)
+                        if(data.code==200){
+                            //location.href='/teacher/teacher_list'
+                            location.href='/teacher/teacher_list'
+                        }
                     }
+                })
 
+            },
 
+            description: {
+                tcName: {
+                    required: '用户名不能为空'
+                },
+                tcPass: {
+                    required: '密码不能为空',
+                    pattern: '密码必须为6位数字'
+                },
+                tcJoinDate: {
+                    required: '日期不能为空'
                 }
-
-            })
-
-
-
+            }
         })
 
-
     }
+
+//    function submitForm(url){
+//        $('#teacherBtn').click(function(){
+//            $.ajax({
+//                type:'post',
+//                url:url,
+//                data:$('#teacherForm').serialize(),
+//                dataType:'json',
+//                success:function(data){
+//                    if(data.code==200){
+//                        location.href='/teacher/teacher_list'
+//                    }
+//
+//
+//                }
+//
+//            })
+//
+//
+//})
+//
+//
+//}
 
 })
